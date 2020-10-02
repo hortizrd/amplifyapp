292d711724133f5a548c661d87e4a9b3994ccacb
Hector Ortiz <hectorortiz1@outlook.com>
2020-10-02 12:54:36 -0400

    https

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -18,7 +18,7 @@ export class SearchForm extends react.Component {
 
           const{inputMovie} = this.state
 
-          fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+          fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
           .then(res => res.json())
           .then(results =>{
               
